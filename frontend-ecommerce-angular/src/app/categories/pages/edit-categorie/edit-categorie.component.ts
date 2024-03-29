5e7b5ec723b392714ac12cdcc84001ca2fbeb307
mboup <mboup124@gmail.com>
2024-03-29 13:04:25 +0100

    add components product + order + category

---
+++ frontend-ecommerce-angular/src/app/categories/pages/edit-categorie/edit-categorie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-categorie',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-categorie.component.html',
+  styleUrl: './edit-categorie.component.css'
+})
+export class EditCategorieComponent {
+
+}
