8f51aa3d2e44736572b3bb2fa082bab55f092479
mboup <mboup124@gmail.com>
2024-06-20 14:30:48 +0200

    products-microservice + spring security +with jwt

--- frontend-ecommerce-angular-authentification-sans-backend/src/app/interfaces/basket.ts
+++
@@ -1,9 +0,0 @@
-import { BasketItem } from "./basket-item";
-
-export interface Basket {
-  id: number;
-  nameBasket: string;
-  totalPrice: number;
-  basketItems: BasketItem[];
-  totalItems: number;
-}
